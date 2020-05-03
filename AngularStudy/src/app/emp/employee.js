bf6b6e13b095cdb9758bb0ded558989c1257e34f
Partey, Manish <manish.partey@duckcreek.com>
2020-05-03 16:49:12 +0530

    Interface 2

--- AngularStudy/src/app/emp/employee.js
+++ AngularStudy/src/app/emp/employee.js
@@ -1,2 +1,16 @@
 "use strict";
+var Employee = (function () {
+    function Employee(empCode, empName, empGender, empASal, empDOB) {
+        this.empCode = empCode;
+        this.empName = empName;
+        this.empGender = empGender;
+        this.empASal = empASal;
+        this.empDOB = empDOB;
+    }
+    Employee.prototype.computeMonthlySalary = function (empASal) {
+        return empASal / 12;
+    };
+    return Employee;
+}());
+exports.Employee = Employee;
 //# sourceMappingURL=employee.js.map
