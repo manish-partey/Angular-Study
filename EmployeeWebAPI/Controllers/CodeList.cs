110a5909d4f19394f3d2816990caea4a02d31f1c
Partey, Manish <manish.partey@duckcreek.com>
2020-05-04 23:44:39 +0530

    Committing Web API Project

---
+++ EmployeeWebAPI/Controllers/CodeList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeWebAPI.Controllers
+{
+	public class CodeList : ApiController
+	{
+		// GET api/<controller>
+		public IEnumerable<DC_PLT_Support_CodeLists> Get()
+		{
+			using (ExampleDataEntities obj =new ExampleDataEntities())
+			{
+				return obj.DC_PLT_Support_CodeLists.ToList();
+			}
+		}
+
+		// GET api/<controller>/5
+		public DC_PLT_Support_CodeLists Get(string code)
+		{
+			using (ExampleDataEntities obj = new ExampleDataEntities())
+			{
+				return obj.DC_PLT_Support_CodeLists.FirstOrDefault(e => e.Code == code);
+			}
+		}
+
+		// POST api/<controller>
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
